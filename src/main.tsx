875c86ab0aa94c5146ba8ae7b795a3f3eddef0a3
Jannik Streek <jannik.streek@gmail.com>
2025-01-27 16:03:26 +0100

    integrate kits changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 import './i18n';
-import LandingPage from './pages/LandingPage';
+import KitsLandingPage from './pages/KitsLandingPage';
 
 const root = document.getElementById('root');
 
@@ -13,7 +13,7 @@ if (root) {
     <StrictMode>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<KitsLandingPage />} />
           <Route path="/document" element={<Navigate to="/" />} />
           <Route path="/document/:id" element={<App />} />
         </Routes>
